799e9d5ff31a232be60e16d14409fb8d5cad9d8e
Renato Wong <renatowong2203@gmail.com>
2023-03-07 12:33:46 -0700

    Fixed Row 1 of the main Grid

--- BikeRide/App.xaml.cs
+++ BikeRide/App.xaml.cs
@@ -4,7 +4,10 @@ public partial class App : Application
 {
 	public App()
 	{
-		InitializeComponent();
+		//Register Syncfusion license
+		Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTI3NDQ4NEAzMjMwMmUzNDJlMzBWcXhLOGwrNXlkYmhvemhaNmVackk1d2lZY1Nvajc5U0h0N0RuZ0l2VSs4PQ==");
+
+        InitializeComponent();
 
 		MainPage = new AppShell();
 	}
